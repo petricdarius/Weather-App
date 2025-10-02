2367feab4ae80c6d883555c5c4ed24d7d36c58e5
petricdarius <badlion30@gmail.com>
2025-10-02 19:03:17 +0300

    Deploy ready

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import weather from "./routes/weather.route.js";
 import dotenv from "dotenv";
-
+import path from "path";
 dotenv.config();
 
 const app = express();
-app.use(express.json()); 
+
+app.use(express.json());
+
 const port = process.env.PORT;
 
+const __dirname = path.resolve();
 app.use("/api", weather);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`SV running on ${port}`);
